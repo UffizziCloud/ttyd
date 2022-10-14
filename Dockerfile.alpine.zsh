0f6f15bc3aed600c18a866c74fab0065b705dbc2
Vibhav Bobade <vibhav.bobde@gmail.com>
2022-10-14 18:28:08 +0530

    fix apk commands

--- Dockerfile.alpine.zsh
+++ Dockerfile.alpine.zsh
@@ -2,7 +2,7 @@ FROM tsl0922/ttyd:alpine
 
 EXPOSE 7681
 
-RUN apk update && apk upgrade -y && apk add -y \
+RUN apk update && apk upgrade && apk add \
       git \
       zsh \
       curl \
@@ -10,8 +10,12 @@ RUN apk update && apk upgrade -y && apk add -y \
       htop \
       wget \
       screen \
-      vim && \
-    wget https://github.com/robbyrussell/oh-my-zsh/raw/master/tools/install.sh -O - | zsh || true && \
-    sed -i 's/ZSH_THEME="robbyrussell"/ZSH_THEME="pygmalion"/' /root/.zshrc
+      vim \ 
+      zsh-autosuggestions \
+      zsh-syntax-highlighting \
+      bind-tools && \
+    sh -c "$(wget https://raw.github.com/robbyrussell/oh-my-zsh/master/tools/install.sh -O -)" && \
+    echo "source /usr/share/zsh/plugins/zsh-syntax-highlighting/zsh-syntax-highlighting.zsh" >> ~/.zshrc && \
+    echo "source /usr/share/zsh/plugins/zsh-autosuggestions/zsh-autosuggestions.zsh" >> ~/.zshrc
 
 # CMD ["ttyd", "/bin/zsh"]
