278f87f16a2bfaca73fc91497b609fcdd98e8b97
Vibhav Bobade <vibhav.bobde@gmail.com>
2022-10-14 18:58:15 +0530

    Add tmux to alpine zsh dockerfile

--- Dockerfile.alpine.zsh
+++ Dockerfile.alpine.zsh
@@ -13,7 +13,8 @@ RUN apk update && apk upgrade && apk add \
       vim \ 
       zsh-autosuggestions \
       zsh-syntax-highlighting \
-      bind-tools && \
+      bind-tools \
+      tmux && \
     sh -c "$(wget https://raw.github.com/robbyrussell/oh-my-zsh/master/tools/install.sh -O -)" && \
     echo "source /usr/share/zsh/plugins/zsh-syntax-highlighting/zsh-syntax-highlighting.zsh" >> ~/.zshrc && \
     echo "source /usr/share/zsh/plugins/zsh-autosuggestions/zsh-autosuggestions.zsh" >> ~/.zshrc
